5f56b3117aec2d468a6196fd89a0516d825e3906
⸸ ᴄʜᴀᴏᴛᴛɪᴄ ⸸ <64570529+chaottic@users.noreply.github.com>
2023-01-24 14:19:10 +1000

    download libraries

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,59 @@
 use std::fs;
 use std::path::Path;
+
 use serde::Deserialize;
 
 #[derive(Deserialize)]
 struct Settings {
-    libraries: Vec<Library>
+    libraries: Libraries
 }
 
 #[derive(Deserialize)]
-struct Library {
+struct Libraries {
+    directory: String,
+    downloads: Vec<Download>
+}
+
+#[derive(Deserialize)]
+struct Download {
     url: String
 }
 
 #[tokio::main]
 async fn main() {
-    let str = fs::read_to_string(Path::new("launcher.json")).unwrap();
+    let file = &*fs::read_to_string(Path::new("settings.json")).unwrap();
 
-    let settings: Settings = serde_json::from_str(&*str).unwrap();
+    let settings: Settings = serde_json::from_str(file).unwrap();
 
-    println!("{}", settings.libraries[0].url);
+    let libraries = settings.libraries;
+
+    let directory = Path::new(&libraries.directory);
+    if !directory.exists() {
+        fs::create_dir_all(directory).expect("Failed to create directories.");
+    }
+
+    for download in &libraries.downloads {
+        if download.url.is_empty() {
+            continue
+        }
+
+        let url = Path::new(&download.url);
+
+        let stem = url.file_stem().unwrap().to_str().unwrap();
+        let extension = url.extension().unwrap().to_str().unwrap();
+
+        let formatted = format!("{}/{}.{}", directory.to_str().unwrap(), stem, extension);
+
+        let path = Path::new(&formatted);
+        if path.exists() {
+            continue;
+        }
+
+        let response = reqwest::get(&download.url).await.unwrap();
+
+        let bytes = response.bytes().await.unwrap();
+
+        fs::write(path, &bytes).expect("Failed to write.");
+    }
 }
+
