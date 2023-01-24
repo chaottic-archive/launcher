633a0d0dd003fbb2b68d603e75c16a670470ec8d
⸸ ᴄʜᴀᴏᴛᴛɪᴄ ⸸ <64570529+chaottic@users.noreply.github.com>
2023-01-24 20:04:25 +1000

    unzip jre

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::fs;
-use std::io::Read;
+use std::{fs, io};
+use std::io::Write;
 use std::path::Path;
 
 use bytes::Bytes;
@@ -15,14 +15,14 @@ struct Settings {
 
 #[derive(Deserialize)]
 struct Jvm {
-    download: Url,
+    url: Url,
     main_class: Url,
     arguments: Vec<String>
 }
 
 #[derive(Deserialize)]
 struct Library {
-    download: Url,
+    url: Url,
     directory: Url
 }
 
@@ -44,21 +44,56 @@ impl Localize for String {
     }
 }
 
+fn download_and_run_jvm(settings: &Settings) {
+    let url = &settings.jvm.url;
+
+    let mut temp_file = tempfile::NamedTempFile::new().expect("Failed to create a temporary file");
+
+    let content = get_content(url).expect("Failed to get jvm archive");
+
+    temp_file.write_all(&*content).expect("Failed to write to temporary file");
+
+    let mut archive = zip::ZipArchive::new(temp_file).expect("Failed to create zip archive.");
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i).expect("Failed to get archive entry");
+
+        let path = file.enclosed_name().to_owned().expect("");
+        if path.to_str().and_then(|str| str.chars().last()).unwrap() == '/' {
+            continue
+        }
+
+        if let Some(p) = path.parent() {
+            if !p.exists() {
+                fs::create_dir_all(p).expect("")
+            }
+        }
+
+        fs::File::create(path).and_then(|mut writer| io::copy(&mut file, &mut writer)).expect("");
+    }
+}
+
 fn get_content(url: &Url) -> reqwest::Result<Bytes> {
     reqwest::blocking::get(url)?.bytes()
 }
 
 fn download_library(library: &Library) {
-    let download = &library.download;
+    let directory = &library.directory;
+    let mut path = Path::new(directory);
 
-    let concat = format!("{}/{}", library.directory, &download.localize().expect("Missing library download url"));
+    if !path.exists() {
+        fs::create_dir_all(path).expect("Failed to create directories");
+    }
 
-    let path = Path::new(&concat);
+    let url = &library.url;
+
+    let concat = format!("{}/{}", library.directory, &url.localize().expect("Missing library download url"));
+
+    path = Path::new(&concat);
     if path.exists() {
         return;
     }
-
-    let content = get_content(&download).expect("");
+    let content = get_content(&url).expect("");
 
     fs::write(path, content).expect("Failed to write downloaded library")
 }
@@ -68,9 +103,11 @@ fn main() {
 
     let settings: Settings = serde_json::from_str(json_file_path).expect("Failed to read json file");
 
-    let libraries = settings.libraries;
+    let libraries = &settings.libraries;
 
-    for library in &libraries {
+    for library in libraries {
         download_library(&library);
     }
+
+    download_and_run_jvm(&settings);
 }
